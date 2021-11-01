7f0b308bbb2009b98d4fcb718781c412771f3c99
Code Sandbox <hello@codesandbox.io>
2021-11-01 22:09:06 +0000

    Deployment from Codesandbox.

---
+++ static/js/main.af815d7f.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpreact=this.webpackJsonpreact||[]).push([[0],{10:function(e,t,c){"use strict";c.r(t);var r=c(1),n=c(3),s=c.n(n),i=(c(8),c(0));function a(){return Object(i.jsxs)("div",{className:"App",children:[(e="Heber",t="Cruz",Object(i.jsxs)("li",{children:[e," ",t]}))," "]});var e,t}var o=document.getElementById("root");s.a.render(Object(i.jsx)(r.StrictMode,{children:Object(i.jsx)(a,{})}),o)},8:function(e,t,c){}},[[10,1,2]]]);
+//# sourceMappingURL=main.af815d7f.chunk.js.map
